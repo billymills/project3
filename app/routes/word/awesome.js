cd3096c9b1788e6296b7348366a398f85b644fb2
Billy Mills <billymills@Billys-MacBook-Pro.local>
2012-04-08 18:53:48 -0400

    getting more than one link on the awesome page

--- app/routes/word/awesome.js
+++ app/routes/word/awesome.js
@@ -11,13 +11,33 @@ var client = redis.createClient();
 exports.index = function(req, res) {
 	//client.zrevrange(['awesomeLink', 0, 0], function(error, linkresult) {
 	
-	client.zrevrange('awesomeLink', 0, 0, function(error, linkresult) { // 0, -1 works
+	
+	
+	client.zrevrange('awesomeLink', 0, 10, function(error, linkresult) { // 0, -1 works
+		var linkArray = new Array();
+		
+		
+		
 		if (error) {
 			console.log (error);
 		}	
-		else {
-			res.render('awesome', {link:linkresult});
-		}	
+		
+		else if (linkresult) {
+			for (i=0; i<=10; i++) {
+				if (linkresult[i]) {
+					linkArray[i]=linkresult[i];
+				} //end if
+				else {
+					linkresult[i] = "";
+				} //end else
+			} //end for	
+				
+		} //end else if		
+		
+		
+			res.render('awesome', {link:linkresult[0],
+									link2:linkresult[1]});
+			
 	});
 };
 
